91e19a2ffa58f36f755b4389adb6521da6bca808
agirard <augirard17@gmail.com>
2024-03-01 00:29:08 +0100

    wip: use action resolver

--- Action/ConvertImage.php
+++ Action/ConvertImage.php
@@ -2,14 +2,12 @@
 
 namespace MrAuGir\Thumbnail\Action;
 
-use MrAuGir\Thumbnail\Action\Input\ConvertImageInput;
-use MrAuGir\Thumbnail\Converter\Resolver\ConverterResolver;
-use MrAuGir\Thumbnail\Engine;
+use MrAuGir\Thumbnail\Action\Output\ConvertImageOutput;
+use MrAuGir\Thumbnail\Command\ConvertImageCommand;
 use MrAuGir\Thumbnail\Exception\ConverterNotFoundException;
 use MrAuGir\Thumbnail\Exception\CreateTmpFileException;
 use MrAuGir\Thumbnail\Exception\ImageConvertException;
 use MrAuGir\Thumbnail\Exception\UnknowSourceImageException;
-use MrAuGir\Thumbnail\Factory\ImageFactory;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +15,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConvertImage
 {
     public function __construct(
-        private readonly Engine            $engine,
-        private readonly ConverterResolver $converterResolver,
+        private readonly InputFactory $inputFactory,
+        private readonly ConvertImageCommand $convertImageFactory
     )
     {}
 
@@ -30,11 +28,10 @@ class ConvertImage
     #[Route("/thumbnail/call/{converter}/{path}", name: "mraugir_thumbnail_converter", requirements: ["path" => ".+" ], methods: ["GET"])]
     public function __invoke(Request $request,string $converter, string $path) : BinaryFileResponse
     {
-        $image = ImageFactory::create($path);
-        $converter = $this->converterResolver->resolve($converter);
+        $input = $this->inputFactory->createFromRequest($converter,$path);
 
-        $outputPath = $this->engine->processConvertion($image,$converter);
+        $outputPath = $this->convertImageFactory->executeFromInput($input);
 
-        return new BinaryFileResponse($outputPath,200, ['Content-Type' => "image/jpeg"]);
+        return (new ConvertImageOutput($outputPath))->getBinaryFileResponse();
     }
 }
--- Action/Output/ConvertImageOutput.php
+++ Action/Output/ConvertImageOutput.php
@@ -2,7 +2,16 @@
 
 namespace MrAuGir\Thumbnail\Action\Output;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
 final class ConvertImageOutput
 {
+    public function __construct(private readonly string $pathOutput)
+    {
+    }
 
+    public function getBinaryFileResponse() : BinaryFileResponse
+    {
+        return new BinaryFileResponse($this->pathOutput,200,["Content-Type" => "image/png"]);
+    }
 }
